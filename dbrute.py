36450c6263024f64a6cc98aa9a4c65b256bfeeda
Palani <84482008+palanioffcl@users.noreply.github.com>
2022-01-20 08:59:15 +0530

    Create dbrute.py

---
+++ dbrute.py
@@ -0,0 +1,18 @@
+import requests
+
+url = input("Enter domain or IP :")
+path = input("Enter Wordlist path :")
+
+file = open(path, "r")
+
+for i in range():
+   dir_name = file.readline()
+   r = requests.get(url+a)
+   stats = r.status_code
+
+if status == 200:
+   print("*"*40)
+   print("Path : "+url+dir_name, (stats))
+   print("*"*40)
+    
+file.close()

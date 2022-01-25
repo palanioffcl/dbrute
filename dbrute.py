51adac6c88e285ad47078e86c78a264041c9340c
Palani <84482008+palanioffcl@users.noreply.github.com>
2022-01-25 12:50:19 +0530

    Update dbrute.py

--- dbrute.py
+++ dbrute.py
@@ -1,18 +1,39 @@
 import requests
 
+print("______________________________________________________________________________")
+print("        _______           __                              __                  ")
+print("       |       \         |  \                            |  \                 ")
+print("       | $$$$$$$\        | $$____    ______   __    __  _| $$_     ______     ")
+print("       | $$  | $$ ______ | $$    \  /      \ |  \  |  \|   $$ \   /      \    ")
+print("       | $$  | $$|      \| $$$$$$$\|  $$$$$$\| $$  | $$ \$$$$$$  |  $$$$$$\   ")
+print("       | $$  | $$ \$$$$$$| $$  | $$| $$|  \$$| $$  | $$| | $$ __ | $$|___|$$  ")
+print("       | $$__/ $$|       | $$__/ $$| $$|     | $$__/ $$| | $$|  \| $$$$$$$$   ")
+print("       | $$    $$|       | $$    $$| $$|      \$$    $$|  \$$  $$ \$$ \____   ")
+print("        \$$$$$$$/         \$$$$$$$/ \$$|        \$$$$$$|   \$$$$   \$$$$$$$|  ")
+print(" __________________________________________________Made by Palani_____________")                                                                    
+                                                                    
+                                                                    
 url = input("Enter domain or IP :")
 path = input("Enter Wordlist path :")
 
 file = open(path, "r")
 
 for i in range():
-   dir_name = file.readline()
-   r = requests.get(url+dir_name)
+   wls = file.readline()
+   r = requests.get(url+wls)
    stats = r.status_code
 
-if status == 200:
-   print("*"*40)
-   print("Path : "+url+dir_name, (stats))
-   print("*"*40)
-    
+if stats == 200:
+   print("_"*50)
+   print("Path :"+url+wls, (stats))
+   print("_"*50)
+elif stats == 404:
+   print("_"*50)
+   print("Path :"+url+wls, (stats))
+   print("_"*50)
+else:
+   print("_"*50)
+   print("Path :"+url+wls, (stats))
+   print("_"*50)
+
 file.close()

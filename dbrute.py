0fb26c295084955fa5293c4f0a865e9102b854de
Palani <palanioffcl@gmail.com>
2022-10-21 20:07:23 +0530

    Update dbrute.py

--- dbrute.py
+++ dbrute.py
@@ -1,19 +1,6 @@
 import requests
-
-print("______________________________________________________________________________")
-print("        _______           __                              __                  ")
-print("       |       \         |  \                            |  \                 ")
-print("       | $$$$$$$\        | $$____    ______   __    __  _| $$_     ______     ")
-print("       | $$  | $$ ______ | $$    \  /      \ |  \  |  \|   $$ \   /      \    ")
-print("       | $$  | $$|      \| $$$$$$$\|  $$$$$$\| $$  | $$ \$$$$$$  |  $$$$$$\   ")
-print("       | $$  | $$ \$$$$$$| $$  | $$| $$|  \$$| $$  | $$| | $$ __ | $$|___|$$  ")
-print("       | $$__/ $$|       | $$__/ $$| $$|     | $$__/ $$| | $$|  \| $$$$$$$$   ")
-print("       | $$    $$|       | $$    $$| $$|      \$$    $$|  \$$  $$ \$$ \____   ")
-print("        \$$$$$$$/         \$$$$$$$/ \$$|        \$$$$$$|   \$$$$   \$$$$$$$|  ")
-print(" __________________________________________________Made by Palani_____________")                                                                    
-                                                                    
-                                                                    
-url = input("Enter domain or IP :")
+                                                       
+url = input("Enter domain or IP with / at end:")
 path = input("Enter Wordlist path :")
 
 file = open(path, "r")
@@ -24,16 +11,16 @@ for i in range(len(file)):
    stats = r.status_code
 
 if stats == 200:
-   print("_"*50)
+   print("="*50)
    print("Path :"+url+wls, (stats))
-   print("_"*50)
+   print("="*50)
 elif stats == 404:
-   print("_"*50)
+   print("="*50)
    print("Path :"+url+wls, (stats))
-   print("_"*50)
+   print("="*50)
 else:
-   print("_"*50)
+   print("="*50)
    print("Path :"+url+wls, (stats))
-   print("_"*50)
+   print("="*50)
 
 file.close()
